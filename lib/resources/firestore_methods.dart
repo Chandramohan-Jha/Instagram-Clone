857701aeffc9d1e20d55b24e7e020849eddee6a6
Chandramohan Jha <chandramohanjha036@gmail.com>
2023-03-20 10:11:24 +0530

    Login signup complete

---
+++ lib/resources/firestore_methods.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FireStoreMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Upload Post
+  Future<String> uploadPost() async {
+    String res = 'Some error Occured';
+    try {} catch (e) {}
+  }
+}
